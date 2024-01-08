3aa34ddc0ae4574628ccc6a18f773d20036e19dc
3MTB <claseseury@gmail.com>
2024-01-08 18:16:22 -0400

    Errores corregidos

--- js/index.js
+++ js/index.js
@@ -1,103 +1,173 @@
-//! getElements HTML
-
-// const $intentosRestantes = document.querySelector("#intentosRestantes span");
-// const $puntosGanar = document.querySelector("#puntosGanar span");
-// const $legend = document.querySelector("form fieldset legend span");
-const $input = document.querySelector("form fieldset label input");
-const $btnSubmit = document.getElementById("btnSubmit");
-// const $btnHelp = document.getElementById("btnHelp");
-// const card = document.getElementById("card");
-
-// const $card = document.getElementById("card");
-// const $infocard = document.querySelector("#card p");
-// const $titleCard = document.querySelector("#card h3");
-// const $contador = document.querySelector("#card p[role=alert] span");
-// const otrosElementos = document.querySelectorAll("body > *:not(#card)");
-// const $btnCloseCard = document.getElementById("btnCloseCard");
-
-//! variables locales
-let numeroEncontrar = 0;
-const premio = [];
-let nivel = 1;
-let puntosAcumulados = 0;
-let intentosRestantes = 0;
-let puntosGanar = 0;
-let avisoTimeout = setTimeout(()=>{},0);
-//! Variables Locales
 try {
-  addEventListener("DOMContentLoaded", () => {
-    muestraCard(
-      "information",
-      "Explicacion del juego",
-      `1- El juego es totalmente aleatorio....
-      <br>2- 
-      Los intentos seran calculados en 
-      base a la dificultad de la ronda
-      .<br>
- 3- Para ganar, debes encontrar el numero antes de agotar tus intentos.<br>
-   4- Cada vez que falles, se 
-   restara un intento y se eliminara el ultimo premio.`,
-      10
-    );
+  //TODO: REVISAR alert
+  //!                Variables Locales
+  const TipoAvisos = {
+    information: "info",
+    warning: "warning",
+    error: "error",
+  };
+  const tipoCard = {
+    information: "information",
+    winner: "winner",
+    loser: "loser",
+  };
+  let cardInterval = setInterval(0);
+  let avisoTimeout = setTimeout(0);
 
+  let numeroEncontrar = 0;
+  const premio = [0];
+  let nivel = 1;
+  let puntosAcumulados = 0;
+  let puntosGanar = 0;
+
+  const $input = document.querySelector("form fieldset label input");
+  const $btnSubmit = document.getElementById("btnSubmit");
+
+  //! --         Variables Locales
+
+  addEventListener("DOMContentLoaded", () => {
+    explicacionJuego();
     iniciaRonda();
+  });
+  addEventListener("click", (e) => {
+    if (e.target === document.getElementById("btnHelp")) {
+      explicacionJuego();
+    } else if (e.target === document.getElementById("btnCloseCard")) {
+      makeCard(null, null, null, true, null);
+    } else if (e.target === $btnSubmit) {
+      if ($input.value.length > 0) {
+        if (parseInt($input.value) === numeroEncontrar) {
+          winner();
+          iniciaRonda();
+        } else {
+          loser();
+        }
+      }
+    }
   });
 
   $input.addEventListener("input", () => {
-    clearTimeout(avisoTimeout);
     if ($input.value.length === 0) {
       $btnSubmit.disabled = true;
       clearTimeout(avisoTimeout);
     } else if (parseInt($input.value) > parseInt($input.getAttribute("max"))) {
       $btnSubmit.disabled = true;
-      creaAviso(
+      makeAviso(
         `El valor introducido debe ser inferior o Igual al maximo establecido: ${$input.getAttribute(
           "max"
         )}`,
-        "warning",
+        TipoAvisos.error,
         3
       );
     } else if (parseInt($input.value) < parseInt($input.getAttribute("min"))) {
       $btnSubmit.disabled = true;
-      creaAviso(
+      makeAviso(
         `El valor introducido debe ser mayor o Igual al minimo establecido: ${$input.getAttribute(
           "min"
         )}`,
-        "warning",
+        TipoAvisos.error,
         3
       );
     } else {
       $btnSubmit.disabled = false;
       clearTimeout(avisoTimeout);
-
-      // clearTimeout(avisoTimeout);
     }
   });
+  //!                   F U N C I O N E S
+  function explicacionJuego() {
+    makeCard(
+      tipoCard.information,
+      "Funcionamiento del JUEGO",
+      10,
+      false,
+      " - El juego consiste en adivinar un numero generado aleatoriamente, entre un rango determinado con una cantidad de intentos determinada.",
+      " - Los intentos seran calculados en base a la longitud de lrango de valores posibles.",
+      " - A mayor Dificultad mayor cantidad de intentos y premios.",
+      " - Cada vez que no aciertes, sete eliminara un intento y un premio.",
+      " - Si logras encontrar el numero, obtendras el premio establecido y pasaras a otra ronda."
+    );
+  }
 
-  addEventListener("click", (e) => {
-    if (e.target === $btnSubmit) {
-      if ($input.value.length > 0) {
-        if (parseInt($input.value) === numeroEncontrar) {
-          ganador();
-          iniciaRonda();
-        } else {
-          perdedor();
+  function makeAviso(message = null, tipo = null, tiempo = null) {
+    const $aviso = document.getElementById("aviso");
+    $aviso.textContent = " ";
+
+    $aviso.classList.remove(...$aviso.classList);
+
+    message = message === null ? " Message Undefined" : message;
+    tipo = tipo === null ? TipoAvisos.information : tipo;
+    tiempo = tiempo === null || typeof tiempo !== "number" ? 3 : tiempo;
+
+    $aviso.classList.add("aviso");
+    $aviso.classList.add(tipo);
+    $aviso.textContent = message;
+    avisoTimeout = setTimeout(() => {
+      $aviso.classList.remove(...$aviso.classList);
+      $aviso.textContent = " ";
+
+      clearTimeout(avisoTimeout);
+    }, tiempo * 1000);
+  }
+
+  function makeCard(
+    tipo = null,
+    title = null,
+    segundos = 3,
+    cancelar = false,
+    ...description
+  ) {
+    const $card = document.getElementById("card");
+    const $otrosElementos = document.querySelectorAll("body > *:not(#card)");
+    $card.classList.remove(...$card.classList);
+
+    if (cancelar) {
+      $card.classList.remove(...$card.classList);
+      $otrosElementos.forEach((elemento) => {
+        if (elemento.tagName.toLowerCase() !== "script") {
+          elemento.style.display = "flex";
         }
-      }
-      actualizaValores();
-    } else if (e.target === document.getElementById("btnHelp")) {
-      muestraCard(
-        "information",
-        "Explicacion del juego  <br><br>",
-        `1- El juego es totalmente aleatorio....<br>2- Los intentos s eran calculados en base a la dificultad de la ronda.<br>
- 3- Para ganar, debes encontrar el numero antes de agotar tus intentos.<br>
-   4- Cada vez que falles, se restara un intento y se eliminara el ultimo premio.`,
-        10
-      );
-    } else if (e.target === document.getElementById("btnCloseCard")) {
-      muestraCard(null, " ", " ", 0, true);
+      });
+      clearInterval(cardInterval);
+      // alert("Card Ocultada");
+      return;
     }
-  });
+    tipo = tipo === null ? tipoCard.information : tipo;
+    title = title === null ? "TITLE UNDEFINED" : title;
+    description =
+      description.length > 0 ? description : "DESCRIPTION UNDEFINED";
+
+    const $title = document.querySelector("#card h3");
+    $title.textContent = title;
+
+    const $description = document.querySelector("#card p");
+    const $contador = document.querySelector("#card p[role=alert] span");
+    $card.classList.add("card");
+    $card.classList.add(tipo);
+    $otrosElementos.forEach((elemento) => {
+      if (elemento.tagName.toLowerCase() !== "script") {
+        elemento.style.display = "none";
+      }
+    });
+    $description.textContent = "";
+    description.forEach((text) => {
+      $description.innerHTML += `${text} <br>`;
+    });
+
+    cardInterval = setInterval(() => {
+      if (segundos <= 0) {
+        $card.classList.remove(...$card.classList);
+        $otrosElementos.forEach((elemento) => {
+          if (elemento.tagName.toLowerCase() !== "script") {
+            elemento.style.display = "flex";
+          }
+        });
+        $description.textContent = "";
+        clearInterval(cardInterval);
+      }
+      --segundos;
+      $contador.textContent = segundos;
+    }, 1000);
+  }
 
   function iniciaRonda() {
     premio.length = 0;
@@ -119,13 +189,12 @@ try {
     else if (rango >= 3) longitudPremio = 2;
     else longitudPremio = 1;
     generaPremio(longitudPremio);
-    // creaAviso("Partida Cargada", "info", 4);
     actualizaValores();
+    makeAviso("Partida Cargada", TipoAvisos.information, 3);
   }
 
   function generaNumeroEncontrar() {
     let max = Math.round(Math.random() * (100 - 3) + 3);
-
     let min = Math.round(Math.random() * (max - 2));
 
     numeroEncontrar = Math.round(Math.random() * (max - min) + min);
@@ -147,7 +216,6 @@ try {
 
   function actualizaValores() {
     const $tablaPuntos = document.querySelector("#tablaPuntos tbody tr");
-    // console.log($tablaPuntos);
     while ($tablaPuntos.firstChild) {
       $tablaPuntos.removeChild($tablaPuntos.firstChild);
     }
@@ -158,21 +226,27 @@ try {
       $tablaPuntos.appendChild(hijo);
       puntosGanar += p;
     });
-    // console.log(document.querySelector("#puntosGanar strong"));
-    // const $tablaPuntos = document.querySelector("#tablaPuntos theaad td #puntosGanar span")
-
-    // document.querySelector("#puntosGanar span").textContent = puntosGanar;
-    // document.querySelector("#intentosRestantes span").textContent =
-    //   premio.length;
+    document.querySelector("#puntosGanar span").textContent = puntosGanar;
+    document.querySelector("#intentosRestantes span").textContent =
+      premio.length;
   }
 
-  function ganador() {
-    actualizaValores();
-    creaAviso(
-      `FELICIDADES HAS GANADO.... +${puntosGanar} puntos... `,
-      "info",
-      3
+  function winner() {
+    makeCard(
+      tipoCard.winner,
+      "Felicidades has ganado",
+      8,
+      false,
+      `Has Ganado ${puntosGanar} 🏆`,
+      "Se cargara una nueva ronda...."
     );
+    actualizaValores();
+    makeAviso(
+      `Felicidades Has Acertado....+${puntosGanar} 🏆`,
+      TipoAvisos.information,
+      2
+    );
+
     document.querySelector("#nivel span").textContent = nivel;
     puntosAcumulados += puntosGanar;
     document.querySelector("#puntosAcumulados span").textContent =
@@ -187,114 +261,44 @@ try {
     );
   }
 
-  function perdedor() {
-    const numeroIngresado = parseInt($input.value);
-    creaAviso(
-      numeroIngresado > numeroEncontrar
-        ? "El numero ingresado es mayor"
-        : "El numero ingresado es menor",
-      "warning",
-      4
-    );
+  function loser() {
     premio.pop();
-
-    actualizaValores();
-    if (premio.length == 0) {
-      creaAviso("Ya no te quedan intentos :😞 Game Over", "error", 4);
-      muestraCard(
-        "loser",
-        "Has perdido 😞😞",
-        `Nivel: ${nivel} Actual... Niveles superados: ${
-          nivel > 1 ? nivel - 1 : 0
-        }
-  Puntos Acumulados: ${puntosAcumulados} +${puntosGanar} Puntos`,
-        3
+    if (premio.length === 0) {
+      makeCard(
+        tipoCard.loser,
+        "Has perdido. ",
+        8,
+        false,
+        "Ya no te quedan intentos",
+        `Niveles superados: ${nivel > 1 ? nivel - 1 : 0}`,
+        `Puntos Acumulados: ${puntosAcumulados}`
       );
       iniciaRonda();
-    }
-  }
-
-  function muestraCard(
-    nameClass = null,
-    titleCard = "error",
-    infoCard = "error",
-    segundos = 3,
-    cancelar = false
-  ) {
-    let cardInterval = setInterval(() => {}, 0);
-
-    const $card = document.getElementById("card");
-    const otrosElementos = document.querySelectorAll("body > *:not(#card)");
-    if (cancelar) {
-      $card.classList.remove(...$card.classList);
-      otrosElementos.forEach((elemento) => {
-        if (elemento.tagName.toLowerCase() !== "script") {
-          elemento.style.display = "flex";
-        }
-      });
-      clearInterval(cardInterval);
+      makeAviso(
+        `Felicidades Has Acertado....+${puntosGanar} 🏆`,
+        TipoAvisos.information,
+        2
+      );
       return;
     }
 
-    const $infocard = document.querySelector("#card p");
-    const $titleCard = document.querySelector("#card h3");
-    const $contador = document.querySelector("#card p[role=alert] span");
+    const numeroIngresado = parseInt($input.value);
+    makeAviso(
+      `El numero ingresado es ${
+        numeroIngresado > numeroEncontrar ? "mayor" : "menor"
+      }`,
+      TipoAvisos.warning,
+      3
+    );
 
-    otrosElementos.forEach((elemento) => {
-      if (elemento.tagName.toLowerCase() !== "script") {
-        elemento.style.display = "none";
-      }
-    });
-    nameClass = nameClass === null ? "loser" : nameClass;
-    $card.classList.remove(...$card.classList);
-
-    $card.classList.add("card");
-    $card.classList.add(nameClass);
-
-    $titleCard.innerHTML = titleCard;
-    $infocard.innerHTML = infoCard;
-
-    cardInterval = setInterval(() => {
-      if (segundos <= 0) {
-        $card.classList.remove(...$card.classList);
-        otrosElementos.forEach((elemento) => {
-          if (elemento.tagName.toLowerCase() !== "script") {
-            elemento.style.display = "flex";
-          }
-        });
-        clearInterval(cardInterval);
-      }
-      --segundos;
-      $contador.textContent = segundos;
-    }, 1000);
-  }
-  function creaAviso(message = null, tipo = "info", tiempo = 2) {
-    clearTimeout(avisoTimeout);
-    if (message.length > 0 && message !== null) {
-      const $aviso = document.getElementById("aviso");
-
-      $aviso.classList.remove(...$aviso.classList);
-      // $aviso.classList.remove("warning");
-      // $aviso.classList.remove("error");
-
-      $aviso.classList.add("aviso");
-      $aviso.classList.add(tipo);
-      $aviso.textContent = message;
-      avisoTimeout = setTimeout(() => {
-        $aviso.classList.remove(...$aviso.classList);
-        // $aviso.classList.remove("info");
-        // $aviso.classList.remove("warning");
-        // $aviso.classList.remove("error");
-        $aviso.setAttribute("hidden", true);
-      }, tiempo * 1000);
-    }
+    actualizaValores();
   }
 } catch (error) {
   alert(
-    `Ha ocurrido un error de nuestra parte :( [${error}]. Reiniciaremos el sistema.... Si el problema persiste, contacta a soporte.`
+    `Ha ocurrido un error de nuestra parte :( [${error}]. Intentaremos Reiniciar el sistema.... Si el problema persiste, contacta a soporte.`
   );
   console.error(
-    `Ha ocurrido un error de nuestra parte :( [${error}]. Reiniciaremos el sistema.... Si el problema persiste, contacta a soporte.`
+    `Ha ocurrido un error de nuestra parte :( [${error} :${error.stack}]. Intentaremos Reiniciar el sistema.... Si el problema persiste, contacta a soporte.`
   );
   iniciaRonda();
 }
